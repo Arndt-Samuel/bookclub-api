a9c35ff3c0fb3a9c4d4f1856a0274c6a8b4cc7a9
Samuel Arndt <samuelarndt@hotmail.com>
2023-10-17 15:57:42 -0300

    feat: forgot and reset password implemented

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,7 +1,9 @@
 import { User } from '../models';
+import { differenceInHours } from 'date-fns'
 import * as Yup from 'yup';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import Mail from '../libs/Mail';
 
 class UserController {
     async login(req, res) {
@@ -69,7 +71,7 @@ class UserController {
             };
 
     
-            const hashPasswords = await bcrypt.hash(req.body.password, 8)
+            const hashPasswords = await bcrypt.hash(req.body.password, 8);
     
             const user = new User({
                 ...req.body,
@@ -99,6 +101,94 @@ class UserController {
             }
     
             return res.json(user);
+        } catch (error) {
+            return res.status(400).json({error: error?.message});
+        }
+    }
+
+    async forgotPassword(req, res){
+        try {
+            const schema = Yup.object().shape({
+                email: Yup.string().email('E-mail invalid').required('E-mail is mandatory'),
+            });
+
+            await schema.validate(req.body);
+
+            const user =  await User.findOne({ where: { email: req.body.email } });
+
+            if(!user){
+                return res.status(404).json({ error: 'User does not exist' })
+            }
+
+    
+    const reset_password_token_sent_at = new Date();
+    const token = Math.random().toString().slice(2, 8);
+    const reset_password_token = await bcrypt.hash(token, 8);
+    
+    await user.update({
+        reset_password_token_sent_at,
+        reset_password_token,
+    });
+
+    const {email, name} = user
+
+    const mailResult = await Mail.sendForgotPasswordMail(email, name, token);
+    
+    if(mailResult?.error){
+        return res.status(400).json({error: 'E-mail not sent'})
+    }
+    
+    return res.json({ success: true })
+
+        } catch (error) {
+            return res.status(400).json({error: error?.message});
+        }
+    }
+
+
+    async resetPassword(req, res) {
+        try {
+            const schema = Yup.object().shape({
+                email: Yup.string().email('E-mail invalid').required('E-mail is mandatory'),
+                token: Yup.string().required('Token is mandatory'),
+                password: Yup.string().required('Password is mandatory').min(6, 'The password must have at least 6 characters'),
+            });
+    
+            await schema.validate(req.body);
+                
+            const user = await User.findOne({ where: { email: req.body.email } });
+    
+            if(!user){
+                return res.status(401).json({ error: 'E-mail or password do not match' });
+            };
+    
+            if(!user.reset_password_token && !user.reset_password_token_sent_at){
+                res.status(400).json({error: 'Password change was not requested'})
+            }
+    
+            const hoursDifference = differenceInHours(new Date(), user.reset_password_token_sent_at );
+
+            if(hoursDifference > 3) {
+                res.status(401).json({error: 'Expired token'})
+            }
+            
+            console.log({hoursDifference});
+    
+            const checkToken = await bcrypt.compare(req.body.token, user.reset_password_token);
+    
+            if(!checkToken) {
+                return res.status(401).json({ error: 'Token invalid!' });
+            };
+            
+            const password_hash = await bcrypt.hash(req.body.password, 8);
+
+            await user.update({
+                password_hash,
+                reset_password_token: null,
+                reset_password_token_sent_at: null,
+            });
+    
+            return res.status(200).json({success: true});
         } catch (error) {
             return res.status(400).json({error: error?.message});
         }
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,13 @@ import UserController from '../controllers/user';
 
 const routes = new Router();
 // ---- unauthenticated routes ----
-routes.post('/user', UserController.create)
-routes.post('/login', UserController.login)
-
+routes.post('/user', UserController.create);
+routes.post('/login', UserController.login);
+routes.post('/forgot-password', UserController.forgotPassword);
+routes.post('/reset-password', UserController.resetPassword);
 
 // ---- authenticated routes ----
 routes.use(authMiddleware);
-routes.get('/user', UserController.get)
+routes.get('/user', UserController.get);
 
 export default routes;
